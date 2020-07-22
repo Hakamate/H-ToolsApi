f0feed74f5b1e81f3c28b0f4b2598952016ef2b1
Hakamate <thomas.clement@viacesi.fr>
2020-07-22 23:12:16 +0200

    download single musique

--- app/Controllers/Http/DownloadController.js
+++ app/Controllers/Http/DownloadController.js
@@ -1,11 +1,8 @@
 'use strict'
 
 const fs = require('fs');
-const path = require('path');
-const https = require('https');
-const ytdl = require('ytdl-core');
-const id = 'https://www.youtube.com/watch?v=QRS8MkLhQmM';
-const lang = 'en';
+const ytdl = require('youtube-dl');
+const youtubedl = require('youtube-dl');
 
 class DownloadController {
     async getMP4({ request, response }) {
@@ -35,64 +32,24 @@ class DownloadController {
     }
 
     download({ request, response }) {
+        let title = null
+        const options = []
         const req = request.all()
         var link = req.params.URL
 
-        // // const target = Helpers.publicPath('video.mp4')
-        // const target = 'video.mp4'
-        // try {
-        //     ytdl.filterFormats(info.formats, 'audioonly');
+        const video = youtubedl(link, ["--format=bestaudio"], {cwd: __dirname});
+       
+        youtubedl.getInfo(link, options, function(err, info){
+            if(err) throw err
+            console.log(info.title)
+            title = info.title
+    
+            video.pipe(
+                response = fs.createWriteStream(title + ".mp3")
+            )
+        })
 
-        //     var stream = ytdl(link)
-        //         .pipe(
-        //             fs.createWriteStream(target)
-        //         );
-
-        //     response.implicitEnd = false
-        //     stream.on('finish', function () {
-        //         response.attachment(target, "test" + '.mp4')
-        //     });
-        // } catch (error) {
-        //     return "test non working"
-        // }
-
-
-
-
-        console.log("gelo")
-
-        ytdl.getInfo(link)
-        .catch(error => {console.log("errorrrrrrrrrrrrrrrrrrrrrrr"); return error})
-        .then(info => {
-            const tracks = info
-                .player_response.captions
-                .playerCaptionsTracklistRenderer.captionTracks;
-            if (tracks && tracks.length) {
-                console.log('Found captions for',
-                    tracks.map(t => t.name.simpleText).join(', '));
-                const track = tracks.find(t => t.languageCode === lang);
-                if (track) {
-                    console.log('Retrieving captions:', track.name.simpleText);
-                    console.log('URL', track.baseUrl);
-                    const output = `${info.videoDetails.title}.${track.languageCode}.xml`;
-                    console.log('Saving to', output);
-                    https.get(track.baseUrl, res => {
-                        res.pipe(fs.createWriteStream(path.resolve(__dirname, output)));
-                    });
-console.log(path.resolve(__dirname, output))
-                    response.implicitEnd = false
-        //     stream.on('finish', function () {
-        //         response.attachment(path.resolve(__dirname, output), "test" + '.mp4')
-        //     });
-                } else {
-                    console.log('Could not find captions for', lang);
-                }
-            } else {
-                console.log('No captions found for this video');
-            }
-        });
-
-
+        console.log('end')
     }
 }
 
